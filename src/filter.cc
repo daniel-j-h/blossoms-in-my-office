81ae89ecfe74c60c3c0d5db9f0c8a6a00ae15e1a
Daniel J. Hofmann <daniel@trvx.org>
2014-11-10 22:23:01 +0100

    Refine HSI ranges to consider valid

--- src/filter.cc
+++ src/filter.cc
@@ -8,10 +8,10 @@ void filter(const cv::Mat& in, cv::Mat& out) {
   // color segmentation in hsv color space for robustness
   cv::cvtColor(in, out, CV_BGR2HSV);
 
-  // red'ish: ~35° left and right of 0° hue; opencv maps values to 8bit (and only uses [0, 179])
+  // red'ish: ~10° left and right of 0° hue; opencv maps values to 8bit (and only uses [0, 179])
   cv::Mat lhs, rhs;
-  cv::inRange(out, cv::Scalar{0, 150, 45}, cv::Scalar{17, 255, 255}, lhs);
-  cv::inRange(out, cv::Scalar{162, 150, 45}, cv::Scalar{255, 255, 255}, rhs);
+  cv::inRange(out, cv::Scalar{0, 128, 85}, cv::Scalar{10, 255, 255}, lhs);
+  cv::inRange(out, cv::Scalar{169, 128, 85}, cv::Scalar{255, 255, 255}, rhs);
 
   // we want both filtered ranges combined
   cv::bitwise_or(lhs, rhs, out);
@@ -20,6 +20,7 @@ void filter(const cv::Mat& in, cv::Mat& out) {
   const double scale{0.01};
   const auto delta = static_cast<int>((std::min)(in.cols, in.rows) * scale);
 
+  // note: size has to be determined for every area; this is only a rough estimation
   const auto struc = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size{delta, delta});
 
   cv::morphologyEx(out, out, cv::MORPH_OPEN, struc);
